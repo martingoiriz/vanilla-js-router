bd0003c532ded73512a454bf7579af76ec82cb9f
Martin Goiriz <martingoiriz@gmail.com>
2021-01-13 15:31:25 -0300

    added hashed based router

--- hashRouter.js
+++ hashRouter.js
@@ -1,11 +1,19 @@
-let navigationCount = 0;
+let firstInternalRoute = false;
 
-window.addEventListener("popstate", event => {
-  navigationCount -= 1;
+(function init() {
+  if (window.location.hash.length > 0) {
+    push(window.location.hash.substring(1));
+    firstInternalRoute = true;
+  } else {
+    reset();
+  }
+})();
 
-  if (navigationCount) {
-    selectTab(event.state.page);
-    renderContent(event.state.page);
+window.addEventListener("popstate", function(e) {
+  if (window.location.hash !== "" && !firstInternalRoute) {
+    let page = window.location.hash.substring(1);
+    selectTab(page);
+    renderContent(page);
   } else {
     reset();
   }
@@ -15,15 +23,14 @@ window.addEventListener("popstate", event => {
 // ======= PUBLIC METHODS =======
 
 function push(page) {
-  navigationCount += 1;
+  window.location.hash = page;
   document.title = page.charAt(0).toUpperCase() + page.slice(1);
   selectTab(page);
   renderContent(page);
-  window.history.pushState({ page }, `${page}`, `${page}`);
 }
 
 function pop() {
-  if (navigationCount !== 0) {
+  if (window.location.hash !== "" && !firstInternalRoute) {
     window.history.back();
   } else {
     reset();
@@ -34,6 +41,10 @@ function pop() {
 // ======= PRIVATE METHODS =======
 
 function renderContent(page) {
+  if (page === "") {
+    reset();
+    return;
+  }
   fetch("/" + page + ".html")
     .then(response => {
       return response.text();
@@ -49,9 +60,11 @@ function selectTab(page) {
     .querySelectorAll(".route")
     .forEach(item => item.classList.remove("selected"));
 
-  document
-    .querySelectorAll("#" + page)
-    .forEach(item => item.classList.add("selected"));
+  if (page !== "") {
+    document
+      .querySelectorAll("#" + page)
+      .forEach(item => item.classList.add("selected"));
+  }
 }
 
 function reset() {
@@ -60,4 +73,6 @@ function reset() {
     .querySelectorAll(".route")
     .forEach(item => item.classList.remove("selected"));
   document.querySelector("#app").innerHTML = "Home";
+  window.location.hash = "#";
+  firstInternalRoute = false;
 }
